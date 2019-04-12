7d5c50fc4711f2b197fb9235d9b31bdd057d77e2
liang <mliang3176@gmail.com>
2019-04-12 03:40:17 +0000

    added PortfolioCards component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
-import ChartComponent from './components/ChartComponent'
-import { Row, Col, Jumbotron, Button } from 'reactstrap';
-import { Card, CardImg, CardText, CardColumns, CardImgOverlay, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
-import video_logo from './video-game.jpg'; 
-import social_logo from './social-media.jpg'; 
-import energy_logo from './energy.jpg'; 
+import ChartComponent from './components/ChartComponent';
+import PortfolioCards from './components/PortfolioCards';
+import { Col } from 'reactstrap';
 
 
 class App extends Component {
@@ -14,35 +11,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Row>
-          <Col sm={{ size: 10, offset: 1 }}>
-            <ChartComponent />
-          </Col>
-        </Row>
-      <Col sm={{ size: 10, offset: 1 }}>
-        <CardColumns>
-          <Card>
-            <CardImg top width="100%" src={social_logo} alt="Card image cap" />
-            <CardBody>
-              <CardTitle>Social Media Portfolio</CardTitle>
-              <Button>Learn More</Button>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg top width="100%" src={video_logo} alt="Card image cap" />
-            <CardBody>
-              <CardTitle>Video Games Portfolio</CardTitle>
-              <Button>Learn More</Button>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg top width="100%" src={energy_logo} alt="Card image cap" />
-            <CardBody>
-              <CardTitle>Energy Portfolio</CardTitle>
-              <Button>Learn More</Button>
-            </CardBody>
-          </Card>
-          </CardColumns>
+        <Col sm={{ size: 10, offset: 1 }}>
+          <ChartComponent />
+        </Col>
+        <Col sm={{ size: 10, offset: 1 }}>
+          <PortfolioCards />
         </Col>
       </div>
     );
---
+++ src/components/PortfolioCards.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Button, Card, CardImg, CardText, CardColumns, CardBody, CardTitle } from 'reactstrap';
+import video_logo from '../video-game.jpg'; 
+import social_logo from '../social-media.jpg'; 
+import energy_logo from '../energy.jpg';
+
+class PortfolioCards extends Component {
+  render() {
+    return (
+      <CardColumns>
+        <Card>
+          <CardImg top width="100%" src={social_logo} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Social Media Portfolio</CardTitle>
+            <Button>Learn More</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg top width="100%" src={video_logo} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Video Games Portfolio</CardTitle>
+            <Button>Learn More</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg top width="100%" src={energy_logo} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Energy Portfolio</CardTitle>
+            <Button>Learn More</Button>
+          </CardBody>
+        </Card>
+        </CardColumns>
+    );
+  }
+}
+
+export default PortfolioCards
