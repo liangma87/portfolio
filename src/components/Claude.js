a6b4077df67de0628247c22500423d6645ffdd63
liangma87 <mliang3176@gmail.com>
2020-02-16 06:39:19 +0000

    Added edit modal in todo dropdown

--- src/components/Claude.js
+++ src/components/Claude.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { ListGroup } from 'reactstrap';
 import Todo from './todo/Todo'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 // Claude represents TODO list
+
+const ModalExample = (props) => {
+  const {
+    buttonLabel,
+    modal,
+    toggle,
+    className
+  } = props;
+
+  return (
+    <div>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
 class Claude extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      todos: []
+      todos: [],
+      modal: false
     };
   }
 
@@ -23,6 +50,10 @@ class Claude extends React.Component {
     .catch((err) => alert(err));
   }
 
+  onEditClick = () => {
+    this.setState({modal: !this.state.modal})
+  }
+
   render() {
     const todos = this.state.todos
     
@@ -35,17 +66,30 @@ class Claude extends React.Component {
       this.props.stocks.length > 1 | this.props.stocks[0].id === todo.company_id);
 
     console.log(selected)
-    return (<ListGroup>
+
+
+    return (
+      <div>
+      <ListGroup>
       {selected.map((todo) => {
         return (
           <Todo
           key = {todo.id}
           {...todo}
           stock = {this.props.stocks.find(stock => stock.id === todo.company_id)}
+          onEditClick = {this.onEditClick}
           />
         )
       })}
-      </ListGroup>);
+      </ListGroup>
+      <ModalExample
+        buttonLabel ="Edit"
+        modal  = {this.state.modal}
+        toggle = {this.onEditClick}
+        className  ="modal-lg"
+        />
+      </div>
+      );
   }
 }
 
--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ListGroupItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
 import { 
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
   Button
+} from 'reactstrap';
+
+import { 
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Input,
+  Label,
+  Form,
+  FormGroup
 } from 'reactstrap';
 
 const Item = ({ ticker, text}) => (
@@ -24,6 +34,33 @@ const Item = ({ ticker, text}) => (
     </div>
   </div>
 )
+
+const ModalExample = (props) => {
+  const {
+    buttonLabel,
+    className
+  } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <div>
+      <Button color="danger" onClick={toggle}>{buttonLabel}</Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle} charCode="Y">Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
 
 class Todo extends React.Component {
   constructor(props) {
@@ -65,6 +102,7 @@ class Todo extends React.Component {
   render() {
 
     return (
+      <div>
       <ListGroupItem
         className="d-flex justify-content-between"
         style ={{wordBreak: 'break-all', minWidth: '50%'}}
@@ -78,17 +116,12 @@ class Todo extends React.Component {
             <DropdownItem onClick={() => this.onDropdownItemClick(this.props.id, 1)}>Push out 1 week</DropdownItem>
             <DropdownItem onClick={() => this.onDropdownItemClick(this.props.id, 2)}>Push out 2 week</DropdownItem>
             <DropdownItem onClick={() => this.onDropdownItemClick(this.props.id, 4)}>Push out 1 month</DropdownItem>
-            <DropdownItem onClick={() => this.onActionClick(0)}>
-                    <Link 
-                      to= {"/addtodo/" + this.props.stock.symbol}
-                    >
-                      Add
-                    </Link>
-            </DropdownItem>
+            <DropdownItem onClick={() => this.props.onEditClick()}> Edit </DropdownItem>
             <DropdownItem onClick={() => this.onActionClick(2)}>Done</DropdownItem>
           </DropdownMenu>
         </UncontrolledDropdown>
       </ListGroupItem>
+      </div>
     );
   }
 
