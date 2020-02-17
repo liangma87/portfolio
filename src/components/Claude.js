9d5af4e4a0546621bef8a9f4b76dff1bd6549781
liangma87 <mliang3176@gmail.com>
2020-02-17 04:23:05 +0000

    added modal's notes/date default value and modifiable

--- src/components/Claude.js
+++ src/components/Claude.js
@@ -9,33 +9,37 @@ const EditModal = (props) => {
   const {
     buttonLabel,
     modal,
-    toggle,
+    onToggleClick,
     className,
     title,
     notes,
-    date
+    date,
+    handleNotesChange,
+    handleDateChange
   } = props;
 
   return (
     <div>
-      <Modal isOpen={modal} toggle={toggle} className={className} >
-        <ModalHeader toggle={toggle}>{title}</ModalHeader>
+      <Modal isOpen={modal} toggle={onToggleClick} className={className} >
+        <ModalHeader toggle={onToggleClick}>{title}</ModalHeader>
         <ModalBody>
           <Input 
             type="textarea" 
             value={notes}
             rows={5}
+            onChange={handleNotesChange}
           />
           <Input
             type="date"
             name="date"
             id="exampleDate"
-            placeholder={date}
+            value={date}
+            onChange={handleDateChange}
           />
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={toggle}>Submit</Button>{' '}
-          <Button color="secondary" onClick={toggle}>Cancel</Button>
+          <Button color="primary" onClick={onToggleClick}>Submit</Button>{' '}
+          <Button color="secondary" onClick={onToggleClick}>Cancel</Button>
         </ModalFooter>
       </Modal>
     </div>
@@ -79,6 +83,15 @@ class Claude extends React.Component {
     this.setState({m_todo_notes: todo_notes});
     this.setState({m_todo_compl_date: todo_completion_date});
   }
+  
+  handleDescrptChange = (event) => {
+    this.setState({m_todo_notes: event.target.value});
+  }
+  
+  handleDateChange = (event) => {
+    console.log(event.target.value)
+    this.setState({m_todo_compl_date: event.target.value});
+  }
 
   render() {
     const todos = this.state.todos
@@ -111,11 +124,13 @@ class Claude extends React.Component {
       <EditModal
         buttonLabel="Edit"
         modal={this.state.modal}
-        toggle={this.onEditClick}
+        onToggleClick={this.onEditClick}
         className="modal-lg"
         title={this.state.m_symbol}
         notes={this.state.m_todo_notes}
         date={this.state.m_todo_compl_date}
+        handleNotesChange={this.handleDescrptChange}
+        handleDateChange={this.handleDateChange}
         />
       </div>
       );
